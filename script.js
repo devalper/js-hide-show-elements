6a53938245927e21c39fe8f50739b74414e1a1d7
Alper SAYICI <alpersayici@gmail.com>
2021-04-08 00:04:13 +0300

    code refactoring

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ const modalButton = document.querySelectorAll('button.show-modal');
 const hidden = document.querySelectorAll('div.hidden');
 const closeButton = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
-
-modalButton.forEach(button => {
-    button.addEventListener('click', function() {
-        hidden.forEach(div => {
-            div.classList.remove('hidden');
-        });
+//Our function for opening the modal
+const openModal = () => {
+    hidden.forEach(div => {
+        div.classList.remove('hidden');
     });
-});
+}
 //Our function for closing the modal
 const closeModal = () => {
     hidden.forEach(div => {
         div.classList.add('hidden');
     });
 }
-//Closing the modal by clicking the close button
+//Opening the modal by clicking the "show modal" buttons
+modalButton.forEach(button => {
+    button.addEventListener('click', openModal);
+});
+//Closing the modal by clicking the close button(X)
 closeButton.addEventListener('click', closeModal);
 //Closing the modal by clicking anywhere outside the model
 overlay.addEventListener('click', closeModal);
