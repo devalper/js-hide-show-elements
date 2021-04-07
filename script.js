c60f0a903571590c0cb1324265ead4f16daf3389
Alper SAYICI <alpersayici@gmail.com>
2021-04-08 00:33:25 +0300

    Add keydown functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const modalButton = document.querySelectorAll('button.show-modal');
 const hidden = document.querySelectorAll('div.hidden');
 const closeButton = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
 //Our function for opening the modal
 const openModal = () => {
     hidden.forEach(div => {
@@ -23,3 +24,9 @@ modalButton.forEach(button => {
 closeButton.addEventListener('click', closeModal);
 //Closing the modal by clicking anywhere outside the model
 overlay.addEventListener('click', closeModal);
+//Closing the modal by pressing the "Esc" key
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    };
+});
